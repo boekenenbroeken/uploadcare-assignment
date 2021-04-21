1cf87e7d627dc92eace83e27df4377791dc1d24e
almost_bergman <almost.bergman@gmail.com>
2021-04-21 12:51:39 +0300

    inject script tag into head & add config endpoint

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 const express = require('express')
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const {
+  createProxyMiddleware,
+  responseInterceptor
+} = require('http-proxy-middleware')
 
 require('dotenv').config()
 
-const { API_SERVICE_URL, UPLOADCARE_PUBLIC_KEY, HOST, PORT } = process.env
+const { ORIGIN_URL, UPLOADCARE_PUBLIC_KEY, HOST, PORT, PROXY_URL } = process.env
 
 const app = express()
 
+app.use(express.static('public'))
+
 app.use(
   '/',
-  createProxyMiddleware({
-    target: API_SERVICE_URL,
+  createProxyMiddleware(['**', '!**/public/*', '!**/config'], {
+    target: ORIGIN_URL,
     changeOrigin: true,
-    onProxyReq: (_proxyReq, req, res) => {
-      if (req.url.match(/\.(gif|jpg|jpeg|tiff?|png|webp|bmp|avif)$/i)) {
-        const imgPath = new URL(req.originalUrl, API_SERVICE_URL).href
+    selfHandleResponse: true,
+    onProxyRes: responseInterceptor(async (responseBuffer, proxyRes) => {
+      if (proxyRes.headers['content-type'].includes('text/html')) {
+        let html = responseBuffer.toString('utf-8')
+        const scriptTag =
+          '<script type="text/javascript" src="/client.js"></script>'
 
-        res.redirect(
-          `https://${UPLOADCARE_PUBLIC_KEY}.ucr.io/-/preview/-/filter/sarnar/${imgPath}`
-        )
+        html = html.replace(/(<head[^>]*>)/, `$1\n${scriptTag}`)
+
+        return Buffer.from(html)
       }
-    }
+
+      return responseBuffer
+    })
   })
 )
+
+app.get('/config', (req, res) => {
+  res.json({
+    uploadcarePublicKey: UPLOADCARE_PUBLIC_KEY,
+    originUrl: ORIGIN_URL,
+    proxyUrl: PROXY_URL
+  })
+})
 
 app.listen(PORT, HOST, () => {
   console.log(`Starting Proxy at ${HOST}:${PORT}`)
